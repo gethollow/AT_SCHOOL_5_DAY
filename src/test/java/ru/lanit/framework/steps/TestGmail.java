f01bea01805c931618c21fb84d3db7f70fe2361e
Hollow <edde.93@mail.ru>
2020-03-28 10:02:00 +0300

    ...

--- src/test/java/ru/lanit/framework/steps/TestGmail.java
+++ src/test/java/ru/lanit/framework/steps/TestGmail.java
@@ -1,13 +1,37 @@
 package ru.lanit.framework.steps;
 
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+import ru.lanit.framework.webdriver.WebDriverManager;
 
-public class TestGmail extends BasePageSteps {
+import java.util.List;
+
+public class TestGmail {
 
     @Test
-    public void testGmail() {
-
+    public void testGoogle() throws InterruptedException {
+        WebDriver webDriver = WebDriverManager.getDriver();
+        webDriver.get("http://google.com");
+        Thread.sleep(1000);
+        WebElement webElement = webDriver.findElement(By.name("q"));
+        List<WebElement> webElements = webDriver.findElements(By.xpath("//div"));
+        webElement.click();
+        Thread.sleep(1000);
+        webElement.sendKeys("Hello World!");
+        Thread.sleep(1000);
+        webElement.sendKeys(Keys.ENTER);
+        Assert.assertTrue(webDriver.findElement(By.xpath("//*[@id='result-stats']")).isDisplayed(), "Элемент \"Результатов: примерно...\"");
+        Thread.sleep(1000);
+        System.out.println("Открыт браузер " + ((ChromeDriver) webDriver).getSessionId());
+        webDriver.quit();
 
     }
 }
